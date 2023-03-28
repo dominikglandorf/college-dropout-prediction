7c8f95919bdf3ba30fc3c1dd683ec71de64158a3
Dominik Glandorf <dominik.glandorf@student.uni-tuebingen.de>
2023-03-28 16:49:14 +0200

    add new imp libraries and first generation composition

--- calculate_course_features.R
+++ calculate_course_features.R
@@ -45,9 +45,36 @@ courses_features <- courses_features %>%
     ttl_owngen_crs=if_else(female==T, ttl_female_crs, ttl_male_crs),
     rel_owngen_crs=if_else(female==T, rel_female_crs, rel_male_crs)
     )
-#print(temp[,c('female','ttl_stu_crs','ttl_female_crs','ttl_male_crs','rel_female_crs','rel_male_crs','ttl_owngen_crs','rel_owngen_crs')],n=150)
+#print(courses_features[,c('female','ttl_stu_crs','ttl_female_crs','ttl_male_crs','rel_female_crs','rel_male_crs','ttl_owngen_crs','rel_owngen_crs')],n=150)
 
 # To do: add first_generation and ethnicity composition. More?
+
+### generate course peer composition: first_generation ###
+# join student first generation status
+courses_features = courses_features %>% 
+  left_join(students %>% select(mellon_id, first_generation))
+
+composition_data <- courses_features %>%  
+  # add n.o. total students and per category in term x course
+  group_by(term_code, course_code) %>% 
+  summarize(ttl_stu_crs = n(),
+            ttl_first_gen_crs = sum(first_generation==T, na.rm=T),
+            ttl_non_first_gen_crs = sum(first_generation==F, na.rm=T)) %>% 
+  # add ratios of females and males
+  mutate(rel_first_gen_crs = ttl_first_gen_crs/ttl_stu_crs,
+         rel_non_first_gen_crs = ttl_non_first_gen_crs/ttl_stu_crs)
+#print(composition_data, n=100)
+
+# join composition data to courses features
+courses_features <- courses_features %>% 
+  left_join(composition_data) %>% 
+  # make own group explicit
+  mutate(
+    ttl_ownfirstgen_crs=if_else(first_generation==T, ttl_first_gen_crs, ttl_non_first_gen_crs),
+    rel_ownfirstgen_crs=if_else(first_generation==T, rel_first_gen_crs, rel_non_first_gen_crs)
+  )
+#print(courses_features[,c('first_generation','ttl_stu_crs','ttl_first_gen_crs','ttl_non_first_gen_crs','rel_first_gen_crs','rel_non_first_gen_crs','ttl_ownfirstgen_crs','rel_ownfirstgen_crs')],n=150)
+
 
 # save to file
 write_csv(courses_features, file.path(path_data, 'course_features_subset.csv'))
